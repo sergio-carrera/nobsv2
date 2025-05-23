e99749c8e5f377a877d957817381fbfcf8c73922
Sergio Carrera <sergiocarrerahidalgo@gmail.com>
2025-05-23 08:32:07 -0600

    Parte 5 lista. Temas vistos: Query String Params, @Query, Spring Data JPA y Repository Methods (JPQL o native)

--- src/main/java/com/example/nobsv2/product/ProductRepository.java
+++ src/main/java/com/example/nobsv2/product/ProductRepository.java
@@ -1,9 +1,24 @@
 package com.example.nobsv2.product;
 
 import com.example.nobsv2.product.model.Product;
+import com.example.nobsv2.product.model.ProductDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+    //Spring Data JPA
+    List<Product> findByNameContaining(String keyword);
+
+    //JPQL
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+    List<Product> findByNameOrDescriptionContaining(@Param("keyword") String name);
+
+    //native query
+    @Query(value = "SELECT * FROM Product WHERE name LIKE %:keyword% OR description LIKE %:keyword%", nativeQuery = true)
+    List<Product> findByNameOrDescriptionContainingNative(@Param("keyword") String keyword);
 }
---
+++ src/main/java/com/example/nobsv2/product/services/SearchProductService.java
@@ -0,0 +1,58 @@
+package com.example.nobsv2.product.services;
+
+import com.example.nobsv2.Query;
+import com.example.nobsv2.product.ProductController;
+import com.example.nobsv2.product.ProductRepository;
+import com.example.nobsv2.product.model.Product;
+import com.example.nobsv2.product.model.ProductDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SearchProductService implements Query<String, List<ProductDTO>> {
+
+    private final ProductRepository productRepository;
+
+    public SearchProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public ResponseEntity<List<ProductDTO>> execute(String name) {
+        return ResponseEntity.ok(productRepository.findByNameOrDescriptionContaining(name)
+                .stream()
+                .map(ProductDTO::new)
+                .toList());
+    }
+}
+
+/*
+Estas 3 opciones hacen lo mismo y son igual de válidas
+
+//Opción 1
+public ResponseEntity<List<ProductDTO>> execute(String name) {
+    return ResponseEntity.ok(productRepository.findByNameContaining(name)
+            .stream()
+            .map(ProductDTO::new)
+            .toList());
+}
+
+//Opción 2
+public ResponseEntity<List<ProductDTO>> execute(String name) {
+    List<Product> products = productRepository.findByNameContaining(name);
+    List<ProductDTO> productDTOS = products.stream().map(ProductDTO::new).toList();
+
+    return ResponseEntity.status(HttpStatus.OK).body(productDTOS);
+}
+
+//Opción 3
+public ResponseEntity<List<ProductDTO>> execute(String name) {
+    return ResponseEntity.status(HttpStatus.OK).body(productRepository.findByNameContaining(name)
+            .stream()
+            .map(ProductDTO::new)
+            .toList());
+}
+*/
